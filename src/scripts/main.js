8685eedd8c2ce0029d6cbfbb4def916681f1145e
Yuliia Babych <jl.babych29@gmail.com>
2025-05-06 22:16:38 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,38 @@
 'use strict';
 
+// function capitalize text
+
+function capitalize(text) {
+  return text.trim().replace(/\b\w/g, (char) => char.toUpperCase());
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const table = document.querySelector('table');
   const thead = table.querySelector('thead');
   const tbody = table.querySelector('tbody');
   let isAscending = true;
   let currenColumn = null;
+
+  // notification function
+  const pushNotification = (title, description, type) => {
+    const message = document.createElement('div');
+    const messageTitle = document.createElement('h2');
+    const messageDescription = document.createElement('p');
+
+    message.classList.add(type);
+    message.setAttribute('data-qa', 'notification');
+
+    messageTitle.innerText = title;
+    messageDescription.innerText = description;
+
+    message.append(messageTitle, messageDescription);
+
+    document.body.append(message);
+
+    setTimeout(() => {
+      message.hidden = true;
+    }, 2000);
+  };
 
   // sorting of the table
   thead.addEventListener('click', (e) => {
@@ -61,7 +88,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const form = document.createElement('form');
 
   form.classList.add('new-employee-form');
-  document.body.appendChild(form);
 
   // adding a label and an input for the name
   const nameLabel = document.createElement('label');
@@ -75,7 +101,6 @@ document.addEventListener('DOMContentLoaded', () => {
   nameInput.setAttribute('name', 'name');
   nameInput.setAttribute('id', 'name');
   nameInput.setAttribute('data-qa', 'name');
-  nameInput.setAttribute('required');
 
   nameLabel.appendChild(nameInput);
   form.appendChild(nameLabel);
@@ -93,44 +118,9 @@ document.addEventListener('DOMContentLoaded', () => {
   positionInput.setAttribute('name', 'position');
   positionInput.setAttribute('id', 'position');
   positionInput.setAttribute('data-qa', 'position');
-  positionInput.setAttribute('required');
 
   positionLabel.appendChild(positionInput);
   form.appendChild(positionLabel);
-
-  // adding a label and an input for the age
-  const ageLabel = document.createElement('label');
-
-  ageLabel.setAttribute('for', 'age');
-  ageLabel.textContent = 'Age:';
-
-  const ageInput = document.createElement('input');
-
-  ageInput.setAttribute('type', 'number');
-  ageInput.setAttribute('name', 'age');
-  ageInput.setAttribute('id', 'age');
-  ageInput.setAttribute('data-qa', 'age');
-  ageInput.setAttribute('required');
-
-  ageLabel.appendChild(ageInput);
-  form.appendChild(ageLabel);
-
-  // adding a label and an input for the salary
-  const salaryLabel = document.createElement('label');
-
-  salaryLabel.setAttribute('for', 'salary');
-  salaryLabel.textContent = 'Salary:';
-
-  const salaryInput = document.createElement('input');
-
-  salaryInput.setAttribute('type', 'number');
-  salaryInput.setAttribute('name', 'salary');
-  salaryInput.setAttribute('id', 'salary');
-  salaryInput.setAttribute('data-qa', 'salary');
-  salaryInput.setAttribute('required');
-
-  salaryLabel.appendChild(salaryInput);
-  form.appendChild(salaryLabel);
 
   // adding select and options
   const selectLabel = document.createElement('label');
@@ -143,7 +133,6 @@ document.addEventListener('DOMContentLoaded', () => {
   select.setAttribute('name', 'office');
   select.setAttribute('id', 'office');
   select.setAttribute('data-qa', 'office');
-  select.setAttribute('required');
 
   const optionsArray = [
     'Tokyo',
@@ -166,10 +155,170 @@ document.addEventListener('DOMContentLoaded', () => {
   selectLabel.appendChild(select);
   form.appendChild(selectLabel);
 
+  // adding a label and an input for the age
+  const ageLabel = document.createElement('label');
+
+  ageLabel.setAttribute('for', 'age');
+  ageLabel.textContent = 'Age:';
+
+  const ageInput = document.createElement('input');
+
+  ageInput.setAttribute('type', 'number');
+  ageInput.setAttribute('name', 'age');
+  ageInput.setAttribute('id', 'age');
+  ageInput.setAttribute('data-qa', 'age');
+
+  ageLabel.appendChild(ageInput);
+  form.appendChild(ageLabel);
+
+  // adding a label and an input for the salary
+  const salaryLabel = document.createElement('label');
+
+  salaryLabel.setAttribute('for', 'salary');
+  salaryLabel.textContent = 'Salary:';
+
+  const salaryInput = document.createElement('input');
+
+  salaryInput.setAttribute('type', 'number');
+  salaryInput.setAttribute('name', 'salary');
+  salaryInput.setAttribute('id', 'salary');
+  salaryInput.setAttribute('data-qa', 'salary');
+
+  salaryLabel.appendChild(salaryInput);
+  form.appendChild(salaryLabel);
+
   // adding a submit button
   const submitButton = document.createElement('button');
 
   submitButton.setAttribute('type', 'submit');
   submitButton.textContent = 'Save to table';
   form.appendChild(submitButton);
+
+  document.body.appendChild(form);
+
+  // adding a new employee to the table by click
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const newName = nameInput.value.trim();
+    const newPosition = positionInput.value.trim();
+    const newOffice = select.value;
+    const newAge = parseFloat(ageInput.value);
+    const newSalary = salaryInput.value;
+
+    // showing errors
+    if (newName.length < 4) {
+      pushNotification(
+        'Error message',
+        'Name must be at least 4 characters long',
+        'error',
+      );
+
+      return;
+    }
+
+    if (!newPosition) {
+      pushNotification(
+        'Error message',
+        'Position must be at least 4 characters long',
+        'error',
+      );
+
+      return;
+    }
+
+    if (newAge < 18 || newAge > 90) {
+      pushNotification(
+        'Error message',
+        'Age must be between 18 and 90.',
+        'error',
+      );
+
+      return;
+    }
+
+    if (newName && newPosition && newOffice && newAge && newSalary) {
+      const newRow = document.createElement('tr');
+
+      // adding a name cell
+      const nameCell = document.createElement('td');
+
+      nameCell.textContent = capitalize(newName);
+      newRow.appendChild(nameCell);
+
+      // adding a position cell
+      const positionCell = document.createElement('td');
+
+      positionCell.textContent = capitalize(newPosition);
+      newRow.appendChild(positionCell);
+
+      // adding an office cell
+      const officeCell = document.createElement('td');
+
+      officeCell.textContent = newOffice;
+      newRow.appendChild(officeCell);
+
+      // adding an age cell or pushing the notification
+      const ageCell = document.createElement('td');
+
+      ageCell.textContent = newAge;
+      newRow.appendChild(ageCell);
+
+      // adding salary sell and format the salary value to a currency format
+      const salaryCell = document.createElement('td');
+
+      salaryCell.textContent = `$${parseFloat(newSalary).toLocaleString(
+        'en-US',
+        {
+          minimumFractionDigits: 0,
+          maximumFractionDigits: 0,
+        },
+      )}`;
+      newRow.appendChild(salaryCell);
+
+      tbody.appendChild(newRow);
+
+      pushNotification(
+        'Success message',
+        'New employee has been successfully added to the table',
+        'success',
+      );
+
+      form.reset();
+    }
+  });
+
+  // editing the cell
+  tbody.addEventListener('dblclick', (e) => {
+    const cell = e.target;
+
+    if (cell.tagName === 'TD') {
+      const originalText = cell.textContent;
+      const newInput = document.createElement('input');
+
+      newInput.setAttribute('type', 'text');
+      newInput.setAttribute('value', originalText);
+      newInput.classList.add('cell-input');
+
+      cell.textContent = '';
+      cell.appendChild(newInput);
+      newInput.focus();
+
+      const save = () => {
+        const newValue = newInput.value.trim();
+
+        cell.textContent = newValue || originalText;
+      };
+
+      newInput.addEventListener('blur', save);
+
+      newInput.addEventListener('keydown', (evt) => {
+        if (evt.key === 'Enter') {
+          evt.preventDefault();
+          newInput.blur();
+        }
+      });
+    }
+  });
 });
